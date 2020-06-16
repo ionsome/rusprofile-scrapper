0f5aae314bfb89dc10c0f5410aad362ac2a4530e
Maxim Adrian <maksim.adrian76@gmail.com>
2020-06-16 18:00:22 +0300

    Обновил получение настроек

--- scrapper.py
+++ scrapper.py
@@ -1,19 +1,36 @@
 #!/bin/python3
 
+import argparse
+import sys
+import os
+
 from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+
 from rusprofile_spider import RusprofileSpider
 
 
-if __name__ == '__main__':
-    settings = {
-        'FEEDS': {
-            'items.json': {'format': 'json'},
-        },
-        'COOKIES_ENABLED': False,
-        'USER_AGENT': 'RedWhite',
-        'FEED_EXPORT_ENCODING': 'utf-8',
-        'DOWNLOAD_DELAY': 1,
-    }
-    process = CrawlerProcess(settings=settings)
-    process.crawl(RusprofileSpider, ids=[89220, 429110])
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Более подробные настройки в setting.py',
+    )
+    parser.add_argument(
+        metavar='id',
+        nargs='+',
+        dest='ids',
+        type=int,
+        help='один/несколько кодов ОКВЭД',
+    )
+    return parser.parse_args()
+
+
+def main():
+    args = parse_args()
+    os.environ['SCRAPY_SETTINGS_MODULE'] = 'settings'
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(RusprofileSpider, ids=args.ids)
     process.start()
+
+
+if __name__ == '__main__':
+    sys.exit(main())
